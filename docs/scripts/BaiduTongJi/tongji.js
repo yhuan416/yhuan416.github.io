5e6edb7976559d2bec0ac770afa607130b7db2b6
yhuan416 <1290635392@qq.com>
2022-02-13 01:18:45 +0000

    test

---
+++ docs/scripts/BaiduTongJi/tongji.js
@@ -0,0 +1,53 @@
+/*
+<script>
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?030e1e62da5455adb0328a84fbf7ec93";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})();
+</script>
+*/
+"use strict";
+
+(function () {
+    function appendScript() {
+      var hm = document.createElement('script');
+      hm.src = "https://hm.baidu.com/hm.js?030e1e62da5455adb0328a84fbf7ec93";
+        document.head.appendChild(hm);
+    //   var s = document.getElementsByTagName("script")[0]; 
+    //   s.parentNode.insertBefore(hm, s);
+    }
+  
+    function init(id) {
+
+      var _hmt = _hmt || [];
+      window._hmt = _hmt;
+
+      appendScript();
+      window.BaiduTongJi =
+        window.BaiduTongJi ||
+        function() {
+          (window.BaiduTongJi.code = window.BaiduTongJi.code || []).push(arguments);
+        };
+    }
+  
+    function collect() {
+      if (!window.BaiduTongJi) {
+        init($docsify.BaiduTongJi);
+      }
+    }
+  
+    var install = function(hook) {
+      if (!$docsify.BaiduTongJi) {
+        console.error('[Docsify] BaiduTongJi is required.');
+        return;
+      }
+  
+      hook.beforeEach(collect);
+    };
+
+    $docsify.plugins = [].concat(install, $docsify.plugins);
+  
+  }());
